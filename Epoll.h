d3627fb5dc9782b6d8642e6e308616c7c3d24d75
SunTianTian2333 <2669516820@qq.com>
2025-02-26 13:40:14 +0800

    add Channel

--- Epoll.h
+++ Epoll.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <sys/epoll.h>
 #include <vector>
-
+#include "Channel.h"
 class Epoll
 {
 private:
@@ -12,6 +12,8 @@ public:
     ~Epoll();
 
     void addFd(int fd, uint32_t op);
-    std::vector<epoll_event> poll(int timeout = -1);
+    // std::vector<epoll_event> poll(int timeout = -1);
+    std::vector<Channel*> poll(int timeout = -1);
+    void updateChannel(Channel*);
 };
 
